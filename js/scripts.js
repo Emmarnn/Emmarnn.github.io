22a4a4f5ef54893b856248f7147f559bc94334b9
Emmanuel <emmarn94@hotmail.fr>
2024-01-25 21:37:15 +0100

    indentation du code

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,61 @@
 /*!
-* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
 //
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector("#mainNav");
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove("navbar-shrink");
+    } else {
+      navbarCollapsible.classList.add("navbar-shrink");
+    }
+  };
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+  // Shrink the navbar
+  navbarShrink();
 
-    };
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      rootMargin: "0px 0px -40%",
     });
+  }
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
     });
+  });
 
+  // Activate SimpleLightbox plugin for portfolio items
+  new SimpleLightbox({
+    elements: "#portfolio a.portfolio-box",
+  });
 });
 
-// function dark mode 
+// function dark mode
 // function myFunction() {
 //   var element = document.body;
 //   element.classList.toggle("dark-mode");
@@ -74,15 +71,19 @@ window.addEventListener('DOMContentLoaded', event => {
 // }
 
 // function textTyped
-var words = ['1) qui je suis.', '2) ce que je fais.', '3) comment me contacter.'],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 20,
-    speed = 80;
+var words = [
+    "1) qui je suis.",
+    "2) ce que je fais.",
+    "3) comment me contacter.",
+  ],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 20,
+  speed = 80;
 var wordflick = function () {
   setInterval(function () {
     if (forwards) {
@@ -93,8 +94,7 @@ var wordflick = function () {
           skip_count = 0;
         }
       }
-    }
-    else {
+    } else {
       if (offset == 0) {
         forwards = true;
         i++;
@@ -108,28 +108,27 @@ var wordflick = function () {
     if (skip_count == 0) {
       if (forwards) {
         offset++;
-      }
-      else {
+      } else {
         offset--;
       }
     }
-    $('.textTyped').text(part);
-  },speed);
+    $(".textTyped").text(part);
+  }, speed);
 };
 
 $(document).ready(function () {
   wordflick();
 });
 
-// function clignotement logos contact 
-    function highlightButton() {
-      var buttons = document.querySelectorAll('.logoFooter');
-    
-      buttons.forEach(function(button) {
-        button.classList.add('highlight');
-  
-        setTimeout(function(){
-          button.classList.remove('highlight');
-        }, 3000);
-      });
-    }
+// function clignotement logos contact
+function highlightButton() {
+  var buttons = document.querySelectorAll(".logoFooter");
+
+  buttons.forEach(function (button) {
+    button.classList.add("highlight");
+
+    setTimeout(function () {
+      button.classList.remove("highlight");
+    }, 3000);
+  });
+}
